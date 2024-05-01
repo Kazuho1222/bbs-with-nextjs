796199713d8021db744be50287b0d1d3ab50d041
Kazuho1222 <destiny5914@gmail.com>
2024-05-01 17:52:07 +0900

    詳細ページ作成

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import BBSCardList from "./components/BBSCardList";
 import { BBSData } from "./types/types";
 
 
-async function getBBSAllData(){
-  const response = await fetch("http://localhost:3000/api/post",{
+async function getBBSAllData() {
+  const response = await fetch("http://localhost:3000/api/post", {
     cache: "no-store",
- });
+  });
 
-  const bbsAllData:BBSData[] = await response.json();
+  const bbsAllData: BBSData[] = await response.json();
 
   return bbsAllData;
 }
+
 export default async function Home() {
   const bbsAllData = await getBBSAllData();
 
