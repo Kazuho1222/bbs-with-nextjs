8b41a35fb29914b38e7266322a1b93c826fc3ca6
Kazuho1222 <destiny5914@gmail.com>
2024-05-01 11:49:57 +0900

    supabase、Prisma実装

--- app/components/BBSCard.tsx
+++ app/components/BBSCard.tsx
@@ -1,24 +1,27 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import React from 'react'
+import { BBSData } from "../types/types";
 
-function BBSCard() {
-  return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, unde cumque porro ratione delectus nostrum recusandae deserunt iusto tenetur consequatur, velit fugit, nihil esse voluptate suscipit optio eos iste rem?
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Link href={"/bbs-posts/1"} className="text-blue-500">Read More</Link>
-        </CardFooter>
-      </Card>
-    </div>
-  )
+interface BBSDataProps {
+  bbsData: BBSData[];
 }
+
+const BBSCard = ({bbsData}:BBSDataProps) => {
+  const {id, title, content, createdAt, username} = bbsData;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{username}</CardDescription>
+      </CardHeader>
+      <CardContent>{content}</CardContent>
+      <CardFooter className="flex justify-between">
+        <Link href={`/bbs-posts/${id}`} className="text-blue-500">Read More</Link>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default BBSCard
--- app/components/BBSCardList.tsx
+++ app/components/BBSCardList.tsx
@@ -1,19 +1,18 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
-import React from 'react'
 import BBSCard from "./BBSCard";
+import { BBSData } from "../types/types";
 
-function BBSCardList() {
+interface BBSAllDataProps {
+  bbsAllData: BBSData[];
+}
+
+const BBSCardList = ({ bbsAllData }: BBSAllDataProps) => {
   return (
     <div className="grid lg:grid-cols-3 px-4 py-4 gap-4">
-      <BBSCard />
-      <BBSCard />
-      <BBSCard />
-      <BBSCard />
-      <BBSCard />
-      <BBSCard />
+      {bbsAllData.map((bbsData: BBSData) => (
+        <BBSCard key={bbsData.id} bbsData={bbsData} />
+      ))}
     </div>
   )
 }
 
-export default BBSCardList
+export default BBSCardList;
